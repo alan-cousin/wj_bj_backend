69fe67bcc9bdc5875e861fa1f26360220ce209b8
alan-cousin <alan_cousin001@yahoo.com>
2022-12-13 04:25:35 -0800

    add double function

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 const port = 2083
 // Add a variable for the api key, address and chain
 const MORALIS_API_KEY = "qUSRFTtgEmYouQteKzxezEdAfhcJ9ZBh1ZsdZUGLtQ6g3RbdBFVBr8SqXfD3Gdhl"
-const bgct_game_factory = "0x3611203aE7551bF519D48cA4901b02bFCde7Ca81";//"0x3D89F0e789000D5b898C36EEf5abAB1cfEfC41E2"
+const bgct_game_factory = "0x470274ddDF0Eb5E43Db8336E30d4E3Aaf9eD34Bb";//"0x3611203aE7551bF519D48cA4901b02bFCde7Ca81";//"0x3D89F0e789000D5b898C36EEf5abAB1cfEfC41E2"
 const owner_pk = "28682cdeadc10d12b5512132ca19e7503b1b61fdfeaa58552972e6b7a052e33c"
 const RPC_URL = "https://matic-mumbai.chainstacklabs.com"
 const game_topic = "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0"
@@ -37,7 +37,7 @@ const startServer = async () => {
         console.log(`Example app listening on port ${port}`)
     })
 }
-async function createGame(user_address, bet_amount) {
+async function createGame(_user, _amount) {
     const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
     const admin_signer = new ethers.Wallet(owner_pk, provider)
     const bj_game_factory = new ethers.Contract(bgct_game_factory, bj_factory_abi, admin_signer)
@@ -45,12 +45,12 @@ async function createGame(user_address, bet_amount) {
     let created_game = ""
     while (!create_success) {
         try {
-            console.log("request create game for " + user_address);
-            const tx_create_game = await bj_game_factory.create(user_address, ethers.utils.parseEther(bet_amount), {gasLimit: 2000000})
-            console.log("create game transaction created for " + user_address)
+            console.log("request create game for " + _user);
+            const tx_create_game = await bj_game_factory.create(_user, ethers.utils.parseEther(_amount), {gasLimit: 2000000})
+            console.log("create game transaction created for " + _user)
             await tx_create_game.wait()
             // Get native balance
-            console.log("create game Transaction processed for " + user_address)
+            console.log("create game Transaction processed for " + _user)
             const transactionHash = tx_create_game.hash;
             console.log("created Transaction hash :" + transactionHash)
             const chain = EvmChain.MUMBAI;
@@ -82,7 +82,38 @@ async function createGame(user_address, bet_amount) {
     console.log("New Game : " + created_game);
     return created_game;
 }
-async function declare_winner(player_address, bjct_game, is_player, is_draw) {
+
+async function doublebet(_game, _user, _amount) {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+    const admin_signer = new ethers.Wallet(owner_pk, provider)
+    const bj_game_factory = new ethers.Contract(bgct_game_factory, bj_factory_abi, admin_signer)
+    let double_bet_success = false;
+    let response_msg = "SUCCESS";
+    while (!double_bet_success) {
+        try {
+            console.log("request double bet for " + _game);
+            const tx_double_bet = await bj_game_factory.doubleBet(_game, _user, ethers.utils.parseEther(_amount), {gasLimit: 2000000})
+            console.log("double bet transaction is created for " + _game)
+            await tx_double_bet.wait()
+            
+            double_bet_success = true;
+        } catch (e) {
+            console.log(e);
+            double_bet_success = true;
+            response_msg = "Unexpected error."
+            const json_body = JSON.parse(e.body);
+            if (json_body.error.message == 'transaction underpriced') {
+                double_bet_success = false;
+            }
+            else{
+                console.log(e.body);
+            }
+        }
+    }
+    return response_msg;
+}
+
+async function declare_winner(_user, _game, _isUserWin, _isDraw) {
     const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
     const admin_signer = new ethers.Wallet(owner_pk, provider)
     const bj_game_factory = new ethers.Contract(bgct_game_factory, bj_factory_abi, admin_signer)
@@ -90,12 +121,12 @@ async function declare_winner(player_address, bjct_game, is_player, is_draw) {
     let tx_msg = "";
     while (!declare_success) {
         try {
-            console.log("request declare winner for game - " + bjct_game)
-            const declare_tx = await bj_game_factory.declareWinner(bjct_game, is_player, is_draw, {gasLimit: 2000000})
-            console.log("declare transaction created - " + bjct_game)
+            console.log("request declare winner for game - " + _game)
+            const declare_tx = await bj_game_factory.declareWinner(_game, _isUserWin, _isDraw, {gasLimit: 2000000})
+            console.log("declare transaction created - " + _game)
             await declare_tx.wait()
             // Get native balance
-            console.log("declare Transaction processed - " + bjct_game)
+            console.log("declare Transaction processed - " + _game)
             const transactionHash = declare_tx.hash;
             console.log("Transaction hash :" + transactionHash)
             
@@ -115,21 +146,21 @@ async function declare_winner(player_address, bjct_game, is_player, is_draw) {
     while (!reward_success) {
         try {
             
-            if(is_player == true){
-                console.log("User won, request collect reward from " + player_address)
-                const bj_game = new ethers.Contract(bjct_game, bj_game_abi, admin_signer)
+            if(_isUserWin == true){
+                console.log("User won, request collect reward from " + _user)
+                const bj_game = new ethers.Contract(_game, bj_game_abi, admin_signer)
                 const collect_tx = await bj_game.collect( {gasLimit: 2000000})
-                console.log("collect transaction created for " + player_address)
+                console.log("collect transaction created for " + _user)
                 await collect_tx.wait()
-                console.log("collect Transaction successed for " + player_address)
+                console.log("collect Transaction successed for " + _user)
             }
-            else if(is_draw == true){
-                console.log("User won, request recover reward from " + player_address)
-                const bj_game = new ethers.Contract(bjct_game, bj_game_abi, admin_signer)
+            else if(_isDraw == true){
+                console.log("Game is drawn, request recover reward from " + _user)
+                const bj_game = new ethers.Contract(_game, bj_game_abi, admin_signer)
                 const recover_tx = await bj_game.recover( {gasLimit: 2000000})
-                console.log("recover transaction created for " + player_address)
+                console.log("recover transaction created for " + _user)
                 await recover_tx.wait()
-                console.log("recover Transaction successed for " + player_address)
+                console.log("recover Transaction successed for " + _user)
             }
             reward_success = true;
         } catch (e) {
@@ -143,13 +174,12 @@ async function declare_winner(player_address, bjct_game, is_player, is_draw) {
         }
     }
     const chain = EvmChain.MUMBAI;
-    const address =  player_address;
     const token_balance = await Moralis.EvmApi.token.getWalletTokenBalances({
-        address:address,
+        address:_user,
         chain:chain,
         tokenAddresses:[bgct_game_token]
     });
-    
+    console.log(token_balance);
     console.log(token_balance.jsonResponse[0].balance);
     return token_balance.jsonResponse[0].balance;
 }
@@ -165,6 +195,30 @@ app.post("/create_game", async (req, res) => {
             bet_amount
         });
         const data = await createGame(acc_address, bet_amount)
+        res.status(200)
+        res.json(data)
+
+    } catch (e) {
+        res.status(200)
+        res.json("Unexpected error.")
+    }
+
+})
+
+app.post("/doublebet", async (req, res) => {
+    console.log("start creating game");
+    // Get and return the crypto data
+    try {
+        const game_address = req.fields.address;
+        const bet_amount = req.fields.amount;
+        const user_acc = req.fields.user;
+        //const { acc_address, bet_amount} = req.query;
+        console.log({
+            game_address,
+            user_acc,
+            bet_amount
+        });
+        const data = await doublebet(game_address, user_acc, bet_amount)
         res.status(200)
         res.json(data)
 
@@ -200,7 +254,6 @@ app.post("/declare_winner", async (req, res) => {
         res.status(200)
         res.json("Unexpected error.")
     }
-
 })
 // Call startServer()
 startServer()
